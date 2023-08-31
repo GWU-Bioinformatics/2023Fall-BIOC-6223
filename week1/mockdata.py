f115847ecee1bf6bbe27d90ac159d5c176b9f89f
kee007ney <12193741+kee007ney@users.noreply.github.com>
2023-08-31 14:14:45 -0400

    Save work

---
+++ week1/mockdata.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+mu, sigma = 150, 1
+
+s = np.random.normal(mu, sigma, 100)
+s = [ '%.2f' % elem for elem in s]
+
+for i in range(1,11):
+	print ("April", str(i) + ",", "2015" + ",", s[i])
+
