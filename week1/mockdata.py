f324171601c4f6f2fc50c1e4cb5ca3b4ad4c471e
skim658 <33474168+seankim658@users.noreply.github.com>
2023-09-10 18:34:44 -0400

    created json

--- week1/mockdata.py
+++ week1/mockdata.py
@@ -1,10 +1,28 @@
 import numpy as np
+import json
+import datetime 
 
 mu, sigma = 150, 1
 
 s = np.random.normal(mu, sigma, 100)
 s = [ '%.2f' % elem for elem in s]
 
-for i in range(1,11):
-	print ("April", str(i) + ",", "2015" + ",", s[i])
+# for i in range(1,11):
+# 	print ("April", str(i) + ",", "2015" + ",", s[i])
 
+date = datetime.date(2015, 4, 1)
+date_delta = datetime.timedelta(days = 1)
+
+data = []
+# print(date.strftime('%B %d, %Y'))
+
+for i in s:
+    data.append({'date': date.strftime('%B %d, %Y'), 'weight': i})
+    date += date_delta
+
+result = {'data': data}
+
+json_result = json.dumps(result)
+
+with open('./week1/kim.json', 'w') as f:
+    f.write(json_result)
